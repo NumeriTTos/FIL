007b76d641101a947385f5fe3b310c993f54e468
APP <124afil@gmail.com>
2026-06-14 12:02:01 +0100

    Import Firestore functions for collection and addDoc
    Added Firestore functions for collection and document addition.

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,10 @@
 // firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { 
+    getFirestore, 
+    collection, 
+    addDoc 
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 export const firebaseConfig = {
   apiKey: "AIzaSyDMwOy94UYwJDtXuOS10PEVwqakwvI8_4w",
@@ -14,3 +18,6 @@ export const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+
+// EXPORTAR FIRESTORE FUNÇÕES
+export { collection, addDoc };
